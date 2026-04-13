2ba2592996656e1443b13b364a5560eff20784fc
Taylor Plewe <tplewe@outlook.com>
2026-04-13 12:09:37 -0600

    display each Unicode block as buttons

--- src/main.zig
+++ src/main.zig
@@ -11,7 +11,7 @@ pub const dvui_app: dvui.App = .{
     .config = .{
         .options = .{
             .size = .{ .w = 800, .h = 600 },
-            .title = "Taylor's Sick DVUI App",
+            .title = "Unimap",
         },
     },
     .initFn = init,
@@ -19,25 +19,33 @@ pub const dvui_app: dvui.App = .{
     .frameFn = frame,
 };
 
-fn comptimeParseUpperHex(comptime T: type, comptime str: []const u8) T {
-    var res: T = 0;
-    var i: isize = str.len - 1;
-    while (i >= 0) : (i -= 1) {
-        const pow = (str.len - i) * 10;
-        res += if (std.ascii.isDigit(str[i]))
-            ((str[i]) - '0') * pow
-        else
-            (((str[i]) - 'A') + 10) * pow;
-    }
-    return res;
-}
-
-fn init(_: *dvui.Window) !void {
-    std.debug.print("num blocks: {d}\n", .{blocks.len});
-    std.debug.print("first range:\n start: {d}\n end: {d}\n", .{ blocks[0].range.start, blocks[0].range.end });
-}
+fn init(_: *dvui.Window) !void {}
 fn deinit() void {}
 fn frame() !dvui.App.Result {
+    {
+        var scaler = dvui.scale(
+            @src(),
+            .{
+                .scale = &dvui.currentWindow().content_scale,
+                .pinch_zoom = .global,
+            },
+            .{ .rect = .cast(dvui.windowRect()) },
+        );
+        defer scaler.deinit();
+
+        var scroll = dvui.scrollArea(
+            @src(),
+            .{},
+            .{ .expand = .both, .style = .window },
+        );
+        defer scroll.deinit();
+
+        for (blocks) |block| {
+            if (dvui.button(@src(), block.name, .{}, .{ .id_extra = block.range.start })) {
+                std.debug.print("clicked: {s}\n", .{block.name});
+            }
+        }
+    }
     return .ok;
 }
 

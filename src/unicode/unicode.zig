ae3cae2f1f398f998c601beefff00caaed80312b
Taylor Plewe <tplewe@outlook.com>
2026-04-13 18:54:15 -0600

    show letters

--- src/unicode/unicode.zig
+++ src/unicode/unicode.zig
@@ -1,3 +1,5 @@
+pub const PHYSICAL_CHAR_VALUE_ALLOC_SIZE = 8;
+
 pub const CodePoint = u21;
 pub const CodePointRange = struct {
     start: CodePoint,
--- src/views/character_list.zig
+++ src/views/character_list.zig
@@ -1,6 +1,8 @@
+const std = @import("std");
 const dvui = @import("dvui");
 
 const App = @import("../App.zig");
+const unicode = @import("../unicode/unicode.zig");
 
 pub fn frame(app: *App) void {
     var scroll = dvui.scrollArea(
@@ -15,7 +17,18 @@ pub fn frame(app: *App) void {
         return;
     }
 
-    // _ = app;
-    // TODO: this is crashing the app
     dvui.labelNoFmt(@src(), app.state.CharacterList.name, .{}, .{});
+
+    {
+        var flex = dvui.flexbox(@src(), .{}, .{ .expand = .horizontal });
+        defer flex.deinit();
+
+        var physical: [unicode.PHYSICAL_CHAR_VALUE_ALLOC_SIZE]u8 = undefined;
+        for (app.state.CharacterList.range.start..app.state.CharacterList.range.end + 1) |i| {
+            const num_bytes = std.unicode.utf8Encode(@intCast(i), &physical) catch unreachable;
+            if (dvui.button(@src(), physical[0..num_bytes], .{}, .{ .id_extra = i })) {
+                std.debug.print("pressed: {}\n", .{i});
+            }
+        }
+    }
 }
